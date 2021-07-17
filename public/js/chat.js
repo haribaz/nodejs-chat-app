74ebb2618f6646d17850f9a4f5bdbc95936e64f5
haribaz <rahuljaya007@gmail.com>
2021-07-17 20:31:31 +0530

    Initial commit

--- public/js/chat.js
+++ public/js/chat.js
@@ -17,25 +17,7 @@ const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true }
 
 const autoscroll = () => {
     // New message element
-    const $newMessage = $messages.lastElementChild
-
-    // Height of the new message
-    const newMessageStyles = getComputedStyle($newMessage)
-    const newMessageMargin = parseInt(newMessageStyles.marginBottom)
-    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
-
-    // Visible height
-    const visibleHeight = $messages.offsetHeight
-
-    // Height of messages container
-    const containerHeight = $messages.scrollHeight
-
-    // How far have I scrolled?
-    const scrollOffset = $messages.scrollTop + visibleHeight
-
-    if (containerHeight - newMessageHeight <= scrollOffset) {
         $messages.scrollTop = $messages.scrollHeight
-    }
 }
 
 socket.on('message', (message) => {
